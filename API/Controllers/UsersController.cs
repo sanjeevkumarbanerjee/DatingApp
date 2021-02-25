d10a78fc445480cdf37a63c62138d58e2ad16319
Sanjeev <sanjeevkumarbanerjee@outlook.com>
2021-02-25 22:29:11 +0530

    Token service with extension method in startup

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,14 +3,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+   
+    public class UsersController : BaseApiController
     {
         private readonly DataContext context;
         public UsersController(DataContext context)
@@ -26,6 +26,7 @@ namespace API.Controllers
         }
 
          [HttpGet("{id}")]
+         [Authorize]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
             return await context.Users.FindAsync(id);
---
+++ API/DTOs/LoginDto.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class LoginDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+
+        [Required]
+        public string Password { get; set; }
+        
+        
+    }
+}
